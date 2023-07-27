3dc1779d787af83246992014c0aff62fac7b48fc
J <59581793+daechi73@users.noreply.github.com>
2023-07-26 22:43:30 -0400

    added footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar.js";
 import About from "./components/sections/About.js";
 import Home from "./components/sections/Home.js";
 import Projects from "./components/sections/Projects";
+import Contact from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
