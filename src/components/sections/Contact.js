bd90b4368255e5982a4f935de576f6cc8c4aeb5c
J <59581793+daechi73@users.noreply.github.com>
2023-07-27 02:34:58 -0400

    added id='contact' for outermost parent container

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -3,7 +3,7 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <>
-      <div className="contact">
+      <div className="contact" id="contact">
         <div className="contacts-title">Contacts:</div>
         <div className="contact-logos">
           <div className="github-logo">
