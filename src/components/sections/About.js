915d8fb484078884f808cd2f6d85e6a95457f45a
J <59581793+daechi73@users.noreply.github.com>
2023-09-14 19:36:51 -0400

    fixed svg attribute names to match react.js

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -198,9 +198,9 @@ const About = () => {
                 y2="43.472"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop offset="0" stop-color="#dfe9f2" />
-                <stop offset=".391" stop-color="#d6e0e9" />
-                <stop offset="1" stop-color="#bfc8d1" />
+                <stop offset="0" stopColor="#dfe9f2" />
+                <stop offset=".391" stopColor="#d6e0e9" />
+                <stop offset="1" stopColor="#bfc8d1" />
               </linearGradient>
               <path
                 fill="url(#QzbsZZGqvt5vTzXzFrTWxa)"
@@ -214,9 +214,9 @@ const About = () => {
                 y2="39.957"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop offset="0" stop-color="#f44f5a" />
-                <stop offset=".443" stop-color="#ee3d4a" />
-                <stop offset="1" stop-color="#e52030" />
+                <stop offset="0" stopColor="#f44f5a" />
+                <stop offset=".443" stopColor="#ee3d4a" />
+                <stop offset="1" stopColor="#e52030" />
               </linearGradient>
               <path
                 fill="url(#QzbsZZGqvt5vTzXzFrTWxb)"
@@ -294,32 +294,32 @@ const About = () => {
               viewBox="0 0 48 48"
               width="64px"
               height="64px"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
             >
               <path
                 fill="#283593"
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M22.903,3.286c0.679-0.381,1.515-0.381,2.193,0 c3.355,1.883,13.451,7.551,16.807,9.434C42.582,13.1,43,13.804,43,14.566c0,3.766,0,15.101,0,18.867 c0,0.762-0.418,1.466-1.097,1.847c-3.355,1.883-13.451,7.551-16.807,9.434c-0.679,0.381-1.515,0.381-2.193,0 c-3.355-1.883-13.451-7.551-16.807-9.434C5.418,34.899,5,34.196,5,33.434c0-3.766,0-15.101,0-18.867 c0-0.762,0.418-1.466,1.097-1.847C9.451,10.837,19.549,5.169,22.903,3.286z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
               <path
                 fill="#5c6bc0"
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M5.304,34.404C5.038,34.048,5,33.71,5,33.255 c0-3.744,0-15.014,0-18.759c0-0.758,0.417-1.458,1.094-1.836c3.343-1.872,13.405-7.507,16.748-9.38 c0.677-0.379,1.594-0.371,2.271,0.008c3.343,1.872,13.371,7.459,16.714,9.331c0.27,0.152,0.476,0.335,0.66,0.576L5.304,34.404z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
               <path
                 fill="#fff"
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M24,10c7.727,0,14,6.273,14,14s-6.273,14-14,14 s-14-6.273-14-14S16.273,10,24,10z M24,17c3.863,0,7,3.136,7,7c0,3.863-3.137,7-7,7s-7-3.137-7-7C17,20.136,20.136,17,24,17z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
               <path
                 fill="#3949ab"
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M42.485,13.205c0.516,0.483,0.506,1.211,0.506,1.784 c0,3.795-0.032,14.589,0.009,18.384c0.004,0.396-0.127,0.813-0.323,1.127L23.593,24L42.485,13.205z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
             <div className="logo-C-Name">C</div>
@@ -422,7 +422,7 @@ const About = () => {
           </div>
           <div className="logo-linux logo">
             <svg width="64" height="64" viewBox="0 0 192.756 192.756">
-              <g fill-rule="evenodd" clip-rule="evenodd">
+              <g fillRule="evenodd" clipRule="evenodd">
                 <path fill="#fff" d="M0 0h192.756v192.756H0V0z" />
                 <path d="M156.98 126.559c-2.275 9.376-13.82 28.953-19.957 37.569-6.135 8.652-5.377 16.441-16.717 13.408-11.305-3.033-14.477-2.481-26.161-1.792-11.615.689-9.099-.345-16.372 2.93-7.238 3.274-31.538-39.706-33.503-47.703-1.93-7.996-2.86-7.031 2.172-15.683S52.198 98.089 58.85 87.61c6.652-10.513 14.339-15.855 13.787-23.886-2.171-29.815-3.895-44.705 9.341-51.598 12.615-6.549 23.162-2.654 27.333-.414 1.791.965 5.445 2.827 8.168 6.101 2.723 3.206 5.17 8.065 6.549 14.201 2.826 12.305-1.172 8.238 2.033 22.335 3.172 14.063 9.617 20.957 17.477 32.09 7.858 11.132 16.062 29.503 13.442 40.12z" />
                 <path
