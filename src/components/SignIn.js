b917c793f0be2ee944f1fad05d3cb7b3ee64be84
J <59581793+daechi73@users.noreply.github.com>
2023-12-21 23:40:41 -0500

    added necessary elements for a sign in system; api fetch from server to authenticate the sign in;

---
+++ src/components/SignIn.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./SignIn.css";
+
+function SignIn() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSignin = () => {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: username, password: password }),
+      mode: "cors",
+    };
+    fetch("http://localhost:3000/users/sign-up", options)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === "success") {
+        }
+      });
+  };
+  const handleUserNameInputChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordInputChange = (e) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <div className="signIn">
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        onChange={handleUserNameInputChange}
+        required
+      />
+      <input
+        type="password"
+        placeholder="passwowrd"
+        name="password"
+        onChange={handlePasswordInputChange}
+        required
+      />
+      <button onClick={handleSignin}>SignIn</button>
+    </div>
+  );
+}
+
+export default SignIn;
