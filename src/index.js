97fb66f5427f6ff62b654e8e5144f874d53879cf
J <59581793+daechi73@users.noreply.github.com>
2023-12-26 19:20:12 -0500

    added routing: app & blog

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import Blog from "./components/pages/blog";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const router = createBrowserRouter([
+  {
+    path: "/Jcodes",
+    element: <App />,
+  },
+  {
+    path: "/Jcodes/blog",
+    element: <Blog />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
