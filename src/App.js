a988f2dde7d54748703ee9508eee949a4027f786
J <59581793+daechi73@users.noreply.github.com>
2023-12-27 10:38:10 -0500

    renders different pages based on useParams

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
 import Navbar from "./components/Navbar.js";
+import Portfolio from "./components/pages/portfolio.js";
+import Blog from "./components/pages/blog.js";
 import About from "./components/sections/About.js";
 import Home from "./components/sections/Home.js";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import Footer from "./components/sections/Footer";
 import SeasonBackground from "./components/SeasonBackground/SeasonBackground";
+import { useParams } from "react-router-dom";
 
 function App() {
+  const { name } = useParams();
+  console.log(name);
   return (
     <>
       <SeasonBackground />
       <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
+      {name === "blog" ? <Blog /> : <Portfolio />}
       <Footer />
     </>
   );
