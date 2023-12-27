0736604c7be0dd1cb5b0f91882ffa869b9f8e9ff
J <59581793+daechi73@users.noreply.github.com>
2023-12-27 10:35:40 -0500

    segregated about, projects, and contacts.

---
+++ src/components/pages/portfolio.js
@@ -0,0 +1,17 @@
+import Home from "../sections/Home";
+import About from "../sections/About";
+import Projects from "../sections/Projects";
+import Contact from "../sections/Contact";
+
+const Portfolio = () => {
+  return (
+    <>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+};
+
+export default Portfolio;
