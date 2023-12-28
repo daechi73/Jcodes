c7b90bdcdfa7421672efbbb2de4fc5bd8072d6f3
J <59581793+daechi73@users.noreply.github.com>
2023-12-28 01:00:51 -0500

    props.content takes in an array as value

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,12 +1,18 @@
 const BlogContent = (props) => {
+  const renderContents = props.content.map((e, i) => {
+    return (
+      <div key={i} className={"blog-content-content " + props.className}>
+        {e}
+      </div>
+    );
+  });
+
   return (
     <div className={"blog-content " + props.className}>
       <div className={"blog-content-title " + props.className}>
         {props.title}
       </div>
-      <div className={"blog-content-content " + props.className}>
-        {props.content}
-      </div>
+      {renderContents}
     </div>
   );
 };
