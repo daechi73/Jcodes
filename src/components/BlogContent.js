9ab6eac44f7b06f366c93be233f3d340b059650e
J <59581793+daechi73@users.noreply.github.com>
2024-01-03 21:48:30 -0500

    added imgs

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -8,11 +8,21 @@ const BlogContent = (props) => {
       </div>
     );
   });
+  const renderImgs = props.imgs.map((e, i) => {
+    return (
+      <div key={i} className={"container-blog-content-img"}>
+        <img src={e.src} alt={e.name} height={e.height} width={e.width} />
+      </div>
+    );
+  });
 
   return (
     <div className={"blog-content " + props.className}>
       <div className={"blog-content-title "}>{props.title}</div>
-      <div className="container-blog-content-content">{renderContents}</div>
+      <div className="container-blog-content-content">
+        <div className="blog-content-contents">{renderContents}</div>
+        <div className="blog-content-imgs">{renderImgs}</div>
+      </div>
     </div>
   );
 };
