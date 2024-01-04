b4de2e781623d748583a3bd37149d01b796c9365
J <59581793+daechi73@users.noreply.github.com>
2024-01-04 00:50:57 -0500

    changed log out success message

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -43,7 +43,7 @@ function SignIn() {
           setSignedIn(false);
           setSignedInUser("");
           changeMessageColor("green");
-          setMessage("success");
+          setMessage("Log out success");
         }
       });
   };
